67142fbb4798b9abaf6c85d3d3ca590317e14ee9
yevhen-kotliarchuk <evgenkotliarchuk@gmail.com>
2024-09-24 22:18:20 +0200

    feat: add responsivnes

--- components/overview/index.tsx
+++ components/overview/index.tsx
@@ -6,13 +6,9 @@ const Overview = () => {
       <span className={styles.greeting_title}>Hey, I&apos;m</span>
       <h1 className={styles.title}>Yevhen Kotliarchuk</h1>
       <article className={styles.description}>
-        Highly skilled and adaptable Software Engineer with 2 years of
-        experience in developing dynamic and user-centric applications. Equipped
-        with expertise in both frontend and backend technologies, I excel at
-        seamlessly bridging the gap between design and functionality. Whether
-        it&apos;s crafting engaging user interfaces or architecting scalable and
-        efficient server-side solutions, I thrive in tackling complex challenges
-        throughout the entire development lifecycle
+        I&apos;m a digital enthusiast with a zest for adventure, dedicated to
+        harnessing technology for creative problem-solving. I enjoy sharing
+        knowledge and fostering connections to inspire growth and innovation.
       </article>
       <div className={styles.btn_ssh_container}>
         <button className={styles.btn_ssh}>
