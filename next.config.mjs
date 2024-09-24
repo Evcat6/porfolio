4749406916c6ff2d3258357575b1ef952e407a39
yevhen-kotliarchuk <evgenkotliarchuk@gmail.com>
2024-09-24 16:24:54 +0200

    fix: add baseUrl in tsconfig.json

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,4 @@
-import path from "node:path";
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack(config) {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      "@": path.resolve(__dirname, "src"),
-    };
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
